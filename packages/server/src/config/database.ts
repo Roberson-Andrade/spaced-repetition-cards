de1e1b1c02ea0236a30c8b2fc00d074d1905d434
Roberson Andrade <roberson_souza11@hotmail.com>
2022-06-13 08:57:31 -0300

    chore(server): Ignore the .env file and add database config

---
+++ packages/server/src/config/database.ts
@@ -0,0 +1,10 @@
+import { PoolConfig } from 'pg';
+
+const clientConfig: PoolConfig = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+};
+
+export default clientConfig;
