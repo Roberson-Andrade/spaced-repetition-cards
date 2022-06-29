5947fca618b3e85db28deeef9c9b40156ce198e7
Roberson Andrade <roberson_souza11@hotmail.com>
2022-06-28 21:35:28 -0300

    feat(server): Use cors with express

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { cors } from 'cors';
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
