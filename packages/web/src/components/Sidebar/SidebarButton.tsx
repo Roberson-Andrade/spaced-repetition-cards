d4de1a99625474fbd6926a7a24ec5df12c38deb3
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-09 17:20:44 -0300

    refactor(web): Refactor the sidebar style and add new props to the button

--- packages/web/src/components/Sidebar/SidebarButton.tsx
+++ packages/web/src/components/Sidebar/SidebarButton.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 type SidebarButtonProps = {
   children: React.ReactNode;
   icon: React.ReactElement;
+  pathLocation: string;
+  onClick: () => void;
   showIcon: boolean;
 }
 
-function SidebarButton({ children, icon, showIcon }: SidebarButtonProps) {
+function SidebarButton({
+  children, icon, showIcon, onClick, pathLocation,
+}: SidebarButtonProps) {
+  const location = useLocation();
+
+  const isActive = location.pathname === pathLocation;
+
   return (
-    <button type="button" className="flex text-center items-center h-[46px] bg-slate-400 text-white hover:bg-slate-500 transition-all p-2">
-      <div className="mr-2">
-        {icon}
+    <button type="button" onClick={onClick} className={`flex h-[40px] transition-all p-2 hover:bg-slate-200 ${isActive ? 'border-r-2 border-solid border-slate-700' : ''}`}>
+      <div className="flex">
+        <div className={`${showIcon ? 'mr-6' : ''} ml-1`}>
+          {icon}
+        </div>
+
+        {showIcon && (
+        <p className="w-[160px] text-start">
+          {children}
+        </p>
+        )}
       </div>
-      {showIcon && children}
     </button>
   );
 }
--- packages/web/src/components/Sidebar/index.tsx
+++ packages/web/src/components/Sidebar/index.tsx
@@ -1,41 +1,67 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BiChevronsLeft, BiMenu } from 'react-icons/bi';
 import { GiCardBurn } from 'react-icons/gi';
+import { IoHomeOutline } from 'react-icons/io5';
+import { useNavigate } from 'react-router-dom';
 import SidebarButton from './SidebarButton';
-
-const buttons = [
-  {
-    text: 'Meus decks',
-    icon: <GiCardBurn size="30px" />,
-  },
-];
 
 function SideBar() {
   const [open, setOpen] = useState(true);
+  const navigate = useNavigate();
 
   const openHandler = () => {
     setOpen((prevState) => !prevState);
   };
 
+  const buttons = [
+    {
+      text: 'Home',
+      icon: <IoHomeOutline size="25px" />,
+      pathLocation: '/home',
+      onClick: () => {
+        navigate('/home');
+      },
+    },
+    {
+      text: 'Meus decks',
+      icon: <GiCardBurn size="25px" />,
+      pathLocation: '/mydecks',
+      onClick: () => {
+        navigate('/mydecks');
+      },
+    },
+  ];
+
   return (
-    <nav className={`flex flex-col min-h-screen transition-all ${open ? 'w-[250px]' : 'w-[50px]'} bg-slate-400`}>
+    <nav className={`flex flex-col min-h-screen transition-all ${open ? 'w-[250px]' : 'w-[50px]'} `}>
       <div className="relative">
-        <h3 className={`text-left text-3xl font-bold text-white p-3 ${!open && 'invisible'}`}>
+        <h3 className={`text-left text-3xl font-bold  p-3 ${!open && 'invisible'}`}>
           Cards
         </h3>
 
         <button
           type="button"
           onClick={openHandler}
-          className={`absolute top-[15px] ${open ? 'right-[-10px] bg-slate-500' : 'right-[50%] translate-x-[50%] bg-slate-400'} w-8 h-8 rounded-full  transition-all hover:bg-slate-700`}
+          className={`absolute top-[15px] ${open ? 'right-[10px]' : 'right-[50%] translate-x-[50%]'} w-8 h-8 rounded-full transition-all hover:bg-slate-200`}
         >
           {open
-            ? <BiChevronsLeft className="text-white w-[100%] h-[100%]" />
-            : <BiMenu className="text-white w-[100%] h-[100%]" />}
+            ? <BiChevronsLeft className=" w-[100%] h-[100%]" />
+            : <BiMenu className=" w-[100%] h-[100%]" />}
         </button>
       </div>
       {buttons.map(
-        ({ text, icon }) => <SidebarButton icon={icon} showIcon={open}>{text}</SidebarButton>,
+        ({
+          text, icon, onClick, pathLocation,
+        }) => (
+          <SidebarButton
+            icon={icon}
+            showIcon={open}
+            pathLocation={pathLocation}
+            onClick={onClick}
+          >
+            {text}
+          </SidebarButton>
+        ),
       )}
     </nav>
   );
