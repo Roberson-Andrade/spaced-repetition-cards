ccde11801f07121f333f1a89c6de9f41ba68fd2d
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-10 20:52:14 -0300

    feat(web): Create the badge component

---
+++ packages/web/src/components/Badge/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/require-default-props */
+import React from 'react';
+
+type BadgeProps = {
+    children?: React.ReactNode;
+}
+
+function Badge({ children }: BadgeProps) {
+  return (
+    <div className="bg- text-[12px] px-2 rounded-full bg-blue-400 shadow-md">
+      <p>{children}</p>
+    </div>
+  );
+}
+
+export default Badge;
