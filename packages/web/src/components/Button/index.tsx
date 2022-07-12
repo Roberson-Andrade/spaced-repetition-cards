74a99b21b2e8ac3826d2d139cae932956285aa88
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-12 20:48:10 -0300

    feat(web): Add disabled property to the button component

--- packages/web/src/components/Button/index.tsx
+++ packages/web/src/components/Button/index.tsx
@@ -1,30 +1,32 @@
-/* eslint-disable react/button-has-type */
-/* eslint-disable react/require-default-props */
-import React from 'react';
-import { selectColorBtn } from './utils';
+import React from "react";
+import { selectColorBtn } from "./utils";
 
 export type ButtonProps = {
-    children: React.ReactNode;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
-    variant?: 'primary' | 'warning' | 'error' | 'success' | 'info';
-    type?: 'button' | 'submit' | 'reset' | undefined;
-    className?: string;
-}
+  children: React.ReactNode;
+  variant?: "primary" | "warning" | "error" | "success" | "info";
+  type?: "button" | "submit" | "reset" | undefined;
+  className?: string;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
 
 function Button({
   children,
   variant,
-  type = 'button',
+  type = "button",
   className,
+  disabled,
   onClick,
 }: ButtonProps) {
   const variantColor = selectColorBtn(variant);
+  const disabledClass = disabled && "btn-disabled";
 
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`px-3 py-2 rounded-md text-white ${variantColor} ${className}`}
+      disabled={disabled}
+      className={`px-3 py-2 rounded-md font-semibold ${disabledClass || variantColor} ${disabledClass} ${className}`}
     >
       {children}
     </button>
--- packages/web/src/components/Button/utils.ts
+++ packages/web/src/components/Button/utils.ts
@@ -1,8 +1,9 @@
-import { ButtonProps } from '.';
+import { ButtonProps } from ".";
 
-export function selectColorBtn(variant: ButtonProps['variant']): string {
+export function selectColorBtn(variant: ButtonProps["variant"]): string {
   switch (variant) {
-    case 'primary': return 'btn-primary';
-    default: return 'btn-primary';
+    case "primary": return "btn-primary";
+    case "info": return "btn-info";
+    default: return "btn-primary";
   }
 }
