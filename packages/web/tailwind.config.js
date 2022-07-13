2d09b5256662c20f3e27372e198e81a703329373
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-12 23:05:21 -0300

    chore(web): Add @kamona/tailwindcss-perspective dependency

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -7,5 +8,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+    require("@kamona/tailwindcss-perspective")
+  ],
 };
