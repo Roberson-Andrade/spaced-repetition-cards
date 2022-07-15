82bf0f8e686b55def42bc0ebcd31599cd8943ad2
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-14 21:15:55 -0300

    feat(web): Add className props

--- packages/web/src/components/Badge/index.tsx
+++ packages/web/src/components/Badge/index.tsx
@@ -3,11 +3,12 @@ import React from "react";
 
 type BadgeProps = {
     children?: React.ReactNode;
+    className?: string;
 }
 
-function Badge({ children }: BadgeProps) {
+function Badge({ children, className }: BadgeProps) {
   return (
-    <div className="text-[12px] px-2 rounded-full bg-blue-400 shadow-md font-semibold text-white">
+    <div className={`text-[12px] px-2 rounded-full bg-blue-400 shadow-md font-semibold text-white ${className}`}>
       <p>{children}</p>
     </div>
   );
