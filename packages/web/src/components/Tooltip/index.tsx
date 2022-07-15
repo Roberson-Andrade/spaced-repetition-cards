7b91c274fe9b996cb947cb6fa73587c69e18fe51
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-14 21:16:25 -0300

    feat(web): Create tooltip component

---
+++ packages/web/src/components/Tooltip/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+type TooltipProps = {
+  children: React.ReactElement;
+  text: string;
+}
+
+function Tooltip({ children, text }: TooltipProps) {
+  const [visible, setVisible] = useState(false);
+
+  const setTooltipVisible = () => {
+    setVisible(true);
+  };
+
+  const setTooltipInvisible = () => {
+    setVisible(false);
+  };
+
+  return (
+    <div className="relative flex-center">
+      <div className={`absolute top-[-2rem] p-1 text-white bg-[#8d8a8a] text-[11px] rounded-md whitespace-nowrap transition-all duration-[400ms] font-medium ${visible ? "opacity-100" : "opacity-0"}`}>
+        <p>{text}</p>
+      </div>
+      <div onMouseEnter={setTooltipVisible} onMouseLeave={setTooltipInvisible}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Tooltip;
