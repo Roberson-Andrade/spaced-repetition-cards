7408115a625730c6aca7657d1fcb2d866b36985b
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-15 09:08:07 -0300

    refactor(web): Use the toggle card prop

--- packages/web/src/containers/Revision/components/RevisionDisplay.tsx
+++ packages/web/src/containers/Revision/components/RevisionDisplay.tsx
@@ -26,41 +26,14 @@ function RevisionDisplay({ cards }: RevisionDisplayProps) {
   return (
     <div className="relative w-full h-full flex-center">
       {cards.map(({
-          id,
-          front,
-          back,
-          deckName,
-          tag,
-          createdAt
-        }, index, array) => {
-          if (index < selected) {
-            return (
-              <Card
-                key={id}
-                front={front}
-                back={back}
-                deckName={deckName}
-                tag={tag}
-                createdAt={createdAt}
-                className="absolute left-[25%] max-h-[500px] max-w-[300px] blur-[1.5px] -rotate-3 transition-transform"
-                rotateDisabled
-                onClick={selectPrevious}
-              />
-            );
-          }
-          if (selected === index) {
-            return (
-              <Card
-                key={id}
-                front={front}
-                back={back}
-                deckName={deckName}
-                tag={tag}
-                createdAt={createdAt}
-                className="absolute max-h-[600px] max-w-[400px] z-[1000] backdrop-blur-sm transition-transform"
-              />
-            );
-          }
+        id,
+        front,
+        back,
+        deckName,
+        tag,
+        createdAt
+      }, index, array) => {
+        if (index < selected) {
           return (
             <Card
               key={id}
@@ -69,12 +42,40 @@ function RevisionDisplay({ cards }: RevisionDisplayProps) {
               deckName={deckName}
               tag={tag}
               createdAt={createdAt}
-              className={`absolute right-[25%] max-h-[500px] max-w-[300px] z-[${(array.length - 1) - index}] blur-[1.5px] rotate-3 transition-transform`}
+              className="absolute left-[25%] max-h-[500px] max-w-[300px] blur-[1.5px] -rotate-3 transition-transform"
               rotateDisabled
-              onClick={selectNext}
+              onClick={selectPrevious}
             />
           );
-        }) }
+        }
+        if (selected === index) {
+          return (
+            <Card
+              key={id}
+              front={front}
+              back={back}
+              deckName={deckName}
+              tag={tag}
+              createdAt={createdAt}
+              flipToggle
+              className="absolute max-h-[600px] max-w-[400px] z-[1000] backdrop-blur-sm transition-transform"
+            />
+          );
+        }
+        return (
+          <Card
+            key={id}
+            front={front}
+            back={back}
+            deckName={deckName}
+            tag={tag}
+            createdAt={createdAt}
+            className={`absolute right-[25%] max-h-[500px] max-w-[300px] z-[${(array.length - 1) - index}] blur-[1.5px] rotate-3 transition-transform`}
+            rotateDisabled
+            onClick={selectNext}
+          />
+        );
+      }) }
     </div>
   );
 }
