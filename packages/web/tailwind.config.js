22d356732a69dbac155ea5e4aad91446c5cf65ae
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-16 18:36:34 -0300

    feat(web): Add new green color as success color

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -6,9 +6,30 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  safelist: [...Array(100)].map((_, index) => `z-[${index}]`),
+  safelist: [
+    ...[...Array(100)].map((_, index) => `z-[${index}]`)
+  ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))"
+      },
+      colors: {
+        success: {
+          50: "#7de775",
+          100: "#73dd6b",
+          200: "#69d361",
+          300: "#5fc957",
+          400: "#55bf4d",
+          500: "#4bb543",
+          600: "#41ab39",
+          700: "#37a12f",
+          800: "#2d9725",
+          900: "#238d1b"
+        }
+      }
+    },
+
   },
   plugins: [
     // eslint-disable-next-line import/no-extraneous-dependencies, global-require
