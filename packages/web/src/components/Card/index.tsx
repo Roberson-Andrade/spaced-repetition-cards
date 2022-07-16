fb68ae53ede39eb351d2556d7231a2dd1c62ff4c
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-15 23:07:44 -0300

    feat(web): Add new props to the card component

--- packages/web/src/components/Card/index.tsx
+++ packages/web/src/components/Card/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
 import Badge from "../Badge";
+import IconButton from "../IconButton";
 
 type CardProps = {
     front: string;
@@ -11,7 +13,12 @@ type CardProps = {
     className?: string;
     rotateDisabled?: boolean;
     flipToggle?: boolean;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    showActionButtons?: boolean;
+    disableFlipAnimation?: boolean;
+    onClickSuccessBtn?: React.MouseEventHandler<HTMLButtonElement>;
+    onClickFailBtn?: React.MouseEventHandler<HTMLButtonElement>;
+    onClick?: React.MouseEventHandler<HTMLDivElement>;
+    onAnimationEnd?: React.AnimationEventHandler<HTMLDivElement>;
 }
 
 function Card({
@@ -23,11 +30,16 @@ function Card({
   className,
   rotateDisabled,
   flipToggle,
-  onClick
+  showActionButtons,
+  disableFlipAnimation,
+  onClick,
+  onClickSuccessBtn,
+  onClickFailBtn,
+  onAnimationEnd
 }: CardProps) {
   const [flip, setFlip] = useState<"rotate-y-0" | "rotate-y-180">("rotate-y-0");
 
-  const clickHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const clickHandler = (event: React.MouseEvent<HTMLDivElement>) => {
     if (onClick) {
       onClick(event);
     }
@@ -47,14 +59,21 @@ function Card({
     }
     setFlip("rotate-y-0");
   };
+
+  useEffect(() => {
+    setFlip("rotate-y-0");
+  }, [rotateDisabled]);
   return (
-    <button
+    <div
       onClick={clickHandler}
       onMouseLeave={leaveFlipHandler}
       onKeyDown={leaveFlipHandler}
       className={`w-full max-w-[250px] h-full max-h-[300px] perspective-10 ${className || ""}`}
+      role="button"
+      tabIndex={0}
+      onAnimationEnd={onAnimationEnd}
     >
-      <div className={`w-full h-full transform-style-3d transition-transform duration-300 ${flip}`}>
+      <div className={`w-full h-full transform-style-3d ${disableFlipAnimation ? "" : "transition-transform duration-300"} ${flip}`}>
         <article
           className="h-full w-full bg-white rounded-md shadow-md hover:shadow-xl hover:translate-y-[-3px] relative transform-style-3d transition-transform cursor-pointer"
         >
@@ -66,7 +85,7 @@ function Card({
               <div className="text-[14px] font-semibold">{deckName}</div>
             </header>
 
-            <div className="text-[15px] p-3">
+            <div className="text-[15px] p-3 text-center">
               {front}
             </div>
 
@@ -83,17 +102,35 @@ function Card({
               <div className="text-[14px] font-semibold">{deckName}</div>
             </header>
 
-            <div className="text-[15px] p-3">
+            <div className="text-[15px] p-3 text-center">
               {back}
             </div>
 
-            <footer className="mt-auto self-end">
+            {showActionButtons && (
+            <div className="flex justify-center gap-2 mt-auto">
+              <IconButton
+                onClick={onClickFailBtn}
+                className="rounded-full hover:-translate-y-1 active:translate-y-0"
+              >
+                <AiFillCloseCircle size="50px" color="#cf142b" />
+
+              </IconButton>
+              <IconButton
+                onClick={onClickSuccessBtn}
+                className="hover:-translate-y-1 active:translate-y-0"
+              >
+                <AiFillCheckCircle size="50px" color="#4BB543" />
+              </IconButton>
+            </div>
+            )}
+
+            <footer className={`${showActionButtons ? "" : "mt-auto"} self-end`}>
               <Badge>{tag}</Badge>
             </footer>
           </div>
         </article>
       </div>
-    </button>
+    </div>
   );
 }
 
