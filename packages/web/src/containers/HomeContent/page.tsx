06cce34902a527d82ec9a9f974ead3e522bd46d3
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-16 18:35:57 -0300

    feat(web): Add the heatmap to the home page

--- packages/web/src/containers/HomeContent/page.tsx
+++ packages/web/src/containers/HomeContent/page.tsx
@@ -1,6 +1,21 @@
+import Button from "../../components/Button";
+import HeatMap from "./components/HeatMap";
+
 function HomeContent() {
   return (
-    <div className="bg-[#F7F8FA] flex-grow">HomeContent</div>
+    <div className="flex-center p-3 flex-col gap-4 bg-[#F7F8FA] flex-grow">
+      <div className="flex-center flex-col gap-3 p-2">
+        <div>
+          Você tem 34 cards a revisar!
+        </div>
+        <Button>
+          Começar revisão
+        </Button>
+      </div>
+      <div className="max-w-4xl">
+        <HeatMap />
+      </div>
+    </div>
   );
 }
 
