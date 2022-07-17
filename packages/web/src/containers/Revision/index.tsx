4f3e470d8e924f5a00896fcbd7dba0579860d57f
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-17 16:07:47 -0300

    feat(web): Create a modal to show up when the revision is completed

--- packages/web/src/containers/Revision/index.tsx
+++ packages/web/src/containers/Revision/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { GiPartyPopper } from "react-icons/gi";
 import Button from "../../components/Button";
+import { useConfirmModal } from "../../hooks/useConfirmModal";
 import { CardType, Deck } from "../../types";
 import RevisionDeck from "./components/RevisionDeck";
 import RevisionDisplay from "./components/RevisionDisplay";
@@ -62,30 +64,44 @@ const mockCards: CardType[] = [
 ];
 function Revision() {
   const [startRevision, setStartRevision] = useState(false);
+  const [modal, createModal] = useConfirmModal();
 
   const startRevisionHandler = () => {
     setStartRevision(true);
   };
 
   const stopRevisionHandler = () => {
+    createModal({
+      title: (
+        <div className="flex items-center justify-between">
+          <h3 className="text-xl font-semibold">Revisão finalizada</h3>
+          <GiPartyPopper size="30px" />
+        </div>
+      ),
+      description: "Parabéns! Você revisou todos os cards deste deck. Crie mais cards e volte para revisá-los!",
+      confirmButtonText: "OK"
+    });
     setStartRevision(false);
   };
   return (
-    <div className="flex-center flex-grow bg-gradient-to-r backdrop-blur-xl from-[#e2e3e4] to-[#acbecd] gap-3 p-5 flex-wrap">
-      <div className="flex flex-col h-full w-full items-center flex-grow ">
-        {!startRevision && (
+    <>
+      <div className="flex-center flex-grow bg-gradient-to-r backdrop-blur-xl from-[#e2e3e4] to-[#acbecd] gap-3 p-5 flex-wrap">
+        <div className="flex flex-col h-full w-full items-center flex-grow ">
+          {!startRevision && (
           <RevisionDeck items={deckItems} onClick={startRevisionHandler} />
-        )}
+          )}
 
-        {startRevision && (
-        <>
-          <RevisionDisplay items={mockCards} stopRevision={stopRevisionHandler} />
-          <Button onClick={stopRevisionHandler}>Parar revisão</Button>
-        </>
-        )}
+          {startRevision && (
+          <>
+            <RevisionDisplay items={mockCards} stopRevision={stopRevisionHandler} />
+            <Button onClick={stopRevisionHandler}>Parar revisão</Button>
+          </>
+          )}
 
+        </div>
       </div>
-    </div>
+      {modal}
+    </>
   );
 }
 
