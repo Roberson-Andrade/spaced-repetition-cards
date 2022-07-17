9e97322b3066827d7df1a3923eed8952f26a4815
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-17 16:06:06 -0300

    refactor(web): Create a variant for the tooltip component

--- packages/web/src/components/Tooltip/index.tsx
+++ packages/web/src/components/Tooltip/index.tsx
@@ -3,10 +3,22 @@ import React, { useState } from "react";
 type TooltipProps = {
   children: React.ReactElement;
   text: string;
+  variant?: "info-xl";
 }
 
-function Tooltip({ children, text }: TooltipProps) {
+function Tooltip({ children, text, variant }: TooltipProps) {
   const [visible, setVisible] = useState(false);
+  let classVariant = "";
+
+  switch (variant) {
+    case "info-xl":
+      classVariant = "text-white bg-black text-[11px] w-[200px] p-1";
+      break;
+
+    default:
+      classVariant = "text-white bg-[#8d8a8a] text-[11px] whitespace-nowrap  p-1 ";
+      break;
+  }
 
   const setTooltipVisible = () => {
     setVisible(true);
@@ -18,7 +30,7 @@ function Tooltip({ children, text }: TooltipProps) {
 
   return (
     <div className="relative flex-center">
-      <div className={`absolute top-[-2rem] p-1 text-white bg-[#8d8a8a] text-[11px] rounded-md whitespace-nowrap transition-all duration-[400ms] font-medium z-50 ${visible ? "opacity-100" : "opacity-0"} pointer-events-none`}>
+      <div className={`absolute top-[-2rem] rounded-md transition-all duration-[400ms] font-medium z-50 ${visible ? "opacity-100" : "opacity-0"} pointer-events-none ${classVariant}`}>
         <p>{text}</p>
       </div>
       <div onMouseEnter={setTooltipVisible} onMouseLeave={setTooltipInvisible}>
