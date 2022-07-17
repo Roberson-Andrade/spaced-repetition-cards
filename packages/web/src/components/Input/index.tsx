f561807269570ee15bb0ebccb6ac93d06df97879
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-17 16:05:27 -0300

    refactor(web): Create a search variant for the input component

--- packages/web/src/components/Input/index.tsx
+++ packages/web/src/components/Input/index.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/require-default-props */
 
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export type InputProps = {
   type: string;
   id: string;
-  label: string;
+  label?: string;
   value: string;
+  variant?: "search";
+  className?: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
@@ -15,10 +18,23 @@ function Input({
   type,
   label,
   value,
+  variant,
+  className,
   onChange,
 }: InputProps) {
+  let classVariant: string;
+
+  switch (variant) {
+    case "search":
+      classVariant = "pl-7";
+      break;
+
+    default:
+      classVariant = "";
+      break;
+  }
   return (
-    <div className="flex flex-col">
+    <div className={`flex flex-col ${className} relative`}>
       <label htmlFor={id} className="text-sm">
         {label}
       </label>
@@ -27,8 +43,13 @@ function Input({
         type={type}
         value={value}
         onChange={onChange}
-        className="rounded-md border focus:outline-none border-blue-200 focus:border-blue-400 focus:border-2 py-1 px-2"
+        className={`rounded-md border focus:outline-none border-blue-200 focus:border-blue-400 focus:border-2 py-1 px-2 ${classVariant}`}
       />
+      {variant === "search" && (
+      <div className="absolute rotate-y-180 left-2 bottom-[8px] pointer-events-none">
+        <AiOutlineSearch className="fill-gray-400" size="20px" />
+      </div>
+      )}
     </div>
   );
 }
