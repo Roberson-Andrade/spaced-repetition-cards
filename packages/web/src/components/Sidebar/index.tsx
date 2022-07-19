f1cf0374ff6460a10e3083256fc1cbd18f312d04
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-19 20:24:52 -0300

    refactor(web): Create a mobile version of the sidebar

--- packages/web/src/components/Sidebar/index.tsx
+++ packages/web/src/components/Sidebar/index.tsx
@@ -5,15 +5,19 @@ import { GiCardBurn, GiPapers } from "react-icons/gi";
 import { HiOutlinePlusSm } from "react-icons/hi";
 import { IoHomeOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import { useWindowWidth } from "../../hooks/useScreenWidth";
 import Button from "../Button";
 import CardForm from "../CardForm";
 import FormModal from "../FormModal";
 import SidebarButton from "./SidebarButton";
+import SidebarMobileButton from "./SidebarMobileButton";
 
 function SideBar() {
   const [openModal, setOpenModal] = useState(false);
   const [open, setOpen] = useState(true);
   const navigate = useNavigate();
+  const screenWidth = useWindowWidth();
+  console.log(screenWidth);
 
   const openModalHandler = () => {
     setOpenModal(true);
@@ -63,51 +67,68 @@ function SideBar() {
 
   return (
     <>
-      <nav
-        className={`flex flex-col min-h-screen transition-all ${
-          open ? "w-[250px]" : "w-[50px]"
-        } `}
-      >
-        <div className="relative">
-          <h3
-            className={`text-left text-3xl font-bold  p-3 ${
-              !open && "invisible"
-            }`}
-          >
-            Cards
-          </h3>
+      {screenWidth > 500 ? (
+        <nav
+          className={`flex flex-col min-h-screen transition-all bg-white ${
+            open ? "w-[250px]" : "w-[50px]"
+          } `}
+        >
+          <div className="relative">
+            <h3
+              className={`text-left text-3xl font-bold  p-3 ${
+                !open && "invisible"
+              }`}
+            >
+              Cards
+            </h3>
 
-          <button
-            type="button"
-            onClick={openHandler}
-            className={`absolute top-[15px] ${
-              open ? "right-[10px]" : "right-[50%] translate-x-[50%]"
-            } w-8 h-8 rounded-full transition-all hover:bg-slate-200`}
-          >
-            {open ? (
-              <BiChevronsLeft className=" w-[100%] h-[100%]" />
-            ) : (
-              <BiMenu className=" w-[100%] h-[100%]" />
-            )}
-          </button>
-        </div>
-        {buttons.map(({
-          text, icon, onClick, pathLocation
-        }) => (
-          <SidebarButton
-            key={text}
-            icon={icon}
-            showIcon={open}
-            pathLocation={pathLocation}
-            onClick={onClick}
-          >
-            {text}
-          </SidebarButton>
-        ))}
-        <Button onClick={openModalHandler} className="mt-auto m-1 flex-center whitespace-nowrap">
-          {open ? "Criar Card" : <HiOutlinePlusSm size="25px" />}
-        </Button>
-      </nav>
+            <button
+              type="button"
+              onClick={openHandler}
+              className={`absolute top-[15px] ${
+                open ? "right-[10px]" : "right-[50%] translate-x-[50%]"
+              } w-8 h-8 rounded-full transition-all hover:bg-slate-200`}
+            >
+              {open ? (
+                <BiChevronsLeft className=" w-[100%] h-[100%]" />
+              ) : (
+                <BiMenu className=" w-[100%] h-[100%]" />
+              )}
+            </button>
+          </div>
+          {buttons.map(({
+            text, icon, onClick, pathLocation
+          }) => (
+            <SidebarButton
+              key={text}
+              icon={icon}
+              showIcon={open}
+              pathLocation={pathLocation}
+              onClick={onClick}
+            >
+              {text}
+            </SidebarButton>
+          ))}
+          <Button onClick={openModalHandler} className="mt-auto m-1 flex-center whitespace-nowrap">
+            {open ? "Criar Card" : <HiOutlinePlusSm size="25px" />}
+          </Button>
+        </nav>
+      ) : (
+        <nav
+          className="fixed bottom-0 w-full flex transition-all h-[40px] bg-white z-20"
+        >
+          {buttons.map(({
+            text, icon, onClick, pathLocation
+          }) => (
+            <SidebarMobileButton
+              key={text}
+              icon={icon}
+              pathLocation={pathLocation}
+              onClick={onClick}
+            />
+          ))}
+        </nav>
+      )}
       <FormModal open={openModal}>
         <CardForm onCloseModal={closeModalHandler} />
       </FormModal>
