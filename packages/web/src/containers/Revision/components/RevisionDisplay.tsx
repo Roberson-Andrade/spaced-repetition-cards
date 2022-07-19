3b161fef822984444edfbe98470b576ba57e90d9
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-19 20:26:52 -0300

    refactor(web): Add the responsive props to the card revision

--- packages/web/src/containers/Revision/components/RevisionDisplay.tsx
+++ packages/web/src/containers/Revision/components/RevisionDisplay.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Card from "../../../components/Card";
+import { useWindowWidth } from "../../../hooks/useScreenWidth";
 import { CardType } from "../../../types";
 
 type RevisionDisplayProps = {
@@ -10,15 +11,18 @@ type RevisionDisplayProps = {
 function RevisionDisplay({ items, stopRevision }: RevisionDisplayProps) {
   const [selected, setSelected] = useState(0);
   const [cards, setCards] = useState(items);
+  const screenWidth = useWindowWidth();
 
-  const selectNext = () => {
+  const selectNext = (event: React.MouseEvent) => {
+    event.stopPropagation();
     if (selected === cards.length) {
       return;
     }
     setSelected((previousState) => previousState + 1);
   };
 
-  const selectPrevious = () => {
+  const selectPrevious = (event: React.MouseEvent) => {
+    event.stopPropagation();
     if (selected === 0) {
       return;
     }
@@ -51,7 +55,7 @@ function RevisionDisplay({ items, stopRevision }: RevisionDisplayProps) {
   }, [cards]);
 
   return (
-    <div className="relative w-full h-full flex-center">
+    <div className="relative w-full h-full 3sm:h-[calc(100vh_-_120px)] flex-center">
       {cards.map(({
         id,
         front,
@@ -60,7 +64,7 @@ function RevisionDisplay({ items, stopRevision }: RevisionDisplayProps) {
         tag,
         createdAt
       }, index, array) => {
-        if (index < selected) {
+        if (index < selected && screenWidth > 600) {
           return (
             <Card
               key={id}
@@ -69,7 +73,7 @@ function RevisionDisplay({ items, stopRevision }: RevisionDisplayProps) {
               deckName={deckName}
               tag={tag}
               createdAt={createdAt}
-              className="absolute left-[25%] max-h-[600px] max-w-[400px] blur-[1.5px] scale-90 -rotate-3"
+              className="absolute left-[20%] 2xl:left-[15%] xl:left-[10%] lg:left-[5%] md:-left-[3.5%] max-h-[600px] max-w-[400px] blur-[1.5px] scale-90 -rotate-3"
               rotateDisabled
               disableFlipAnimation
               onClick={selectPrevious}
@@ -87,17 +91,21 @@ function RevisionDisplay({ items, stopRevision }: RevisionDisplayProps) {
               createdAt={createdAt}
               flipToggle
               showActionButtons
+              showLeftButton={index !== 0 && screenWidth < 600}
+              showRightButton={index !== array.length - 1 && screenWidth < 600}
               onClickSuccessBtn={(event) => {
                 successClickHandler(event, index);
               }}
               onClickFailBtn={(event) => {
                 failClickHandler(event, index);
               }}
+              onClickNextBtn={selectNext}
+              onClickPreviousBtn={selectPrevious}
               className="absolute max-h-[600px] max-w-[400px] z-[1000] backdrop-blur-sm"
             />
           );
         }
-        return (
+        return screenWidth > 600 ? (
           <Card
             key={id}
             front={front}
@@ -105,12 +113,12 @@ function RevisionDisplay({ items, stopRevision }: RevisionDisplayProps) {
             deckName={deckName}
             tag={tag}
             createdAt={createdAt}
-            className={`absolute right-[25%] max-h-[600px] max-w-[400px] z-[${(array.length - 1) - index}] scale-90 blur-[1.5px] rotate-3`}
+            className={`absolute right-[20%] 2xl:right-[15%] xl:right-[10%] lg:right-[5%] md:-right-[3.5%] max-h-[600px] max-w-[400px] z-[${(array.length - 1) - index}] scale-90 blur-[1.5px] rotate-3`}
             rotateDisabled
             disableFlipAnimation
             onClick={selectNext}
           />
-        );
+        ) : null;
       }) }
     </div>
   );
