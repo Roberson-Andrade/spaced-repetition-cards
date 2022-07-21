e25c65ddec280a0b5ec45daaea08ab3eac0e1c55
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-21 08:43:02 -0300

    feat(web): Fetch the deck data

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import AppContent from "./app.routes";
 import SideBar from "./components/Sidebar";
+import { useStore } from "./store";
 
 function App() {
+  const fetchDecks = useStore((state) => state.fetchDecks);
+
+  useEffect(() => {
+    fetchDecks();
+  }, []);
   return (
     <div className="flex text-slate-700">
       <SideBar />
