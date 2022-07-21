3e1e0ffdca062813994addde3095af93ae61bac3
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-21 08:41:33 -0300

    feat(web): Replace the mock deck data

--- packages/web/src/containers/Revision/components/RevisionDeck.tsx
+++ packages/web/src/containers/Revision/components/RevisionDeck.tsx
@@ -1,27 +1,33 @@
 import React from "react";
+import { useStore } from "../../../store";
 import { Deck } from "../../../types";
 import RevisionDeckItem from "./RevisionDeckItem";
 
 type RevisionDeckProps = {
   items: Deck[]
-  onClick: React.MouseEventHandler<HTMLButtonElement>
+  onClick: (event: React.MouseEvent<HTMLButtonElement>, index: number) => void;
 }
 
 function RevisionDeck({ items, onClick }: RevisionDeckProps) {
+  const cards = useStore((state) => state.cards);
   return (
     <div className="h-full w-full max-w-[1000px] p-5 mx-5 rounded-md">
       <div className="flex items-center justify-between">
         <h3 className="text-xl">Decks</h3>
         <p className="flex font-medium">
-          Total de cards: 200
+          Total de cards:
+          {" "}
+          {cards.length}
         </p>
       </div>
       <ul className="mt-4 h-[85vh] max-h-full overflow-y-auto">
-        {items.map((deck) => (
+        {items.map((deck, i) => (
           <RevisionDeckItem
             key={deck.id}
             item={deck}
-            onClick={onClick}
+            onClick={(event) => {
+              onClick(event, i);
+            }}
           />
         ))}
       </ul>
