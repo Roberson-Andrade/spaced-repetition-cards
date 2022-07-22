2d21b7c575f95af3f303fbe51d8167bae1478773
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-22 18:14:41 -0300

    feat(web): Add loading to the first fetch

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,18 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { VscLoading } from "react-icons/vsc";
+import { toast, ToastContainer } from "react-toastify";
 import AppContent from "./app.routes";
 import SideBar from "./components/Sidebar";
+import { defaultToast } from "./constants/toastify";
 import { useStore } from "./store";
 
 function App() {
   const fetchDecks = useStore((state) => state.fetchDecks);
+  const [loading, setLoading] = useState(true);
 
+  const stopLoadingHandler = (error: string | null) => {
+    setLoading(false);
+    if (error) {
+      toast.error(error || "Erro interno", defaultToast);
+    }
+  };
   useEffect(() => {
-    fetchDecks();
+    fetchDecks(stopLoadingHandler);
   }, []);
   return (
     <div className="flex text-slate-700">
-      <SideBar />
-      <AppContent />
+      {loading ? (
+        <div className="w-full min-h-[100vh] flex justify-center items-center">
+          <VscLoading className="animate-spin" size="100px" />
+        </div>
+      ) : (
+        <>
+          <SideBar />
+          <AppContent />
+          <ToastContainer />
+        </>
+      )}
     </div>
   );
 }
