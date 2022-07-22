ddbfd4736142eba4927708a39c86ce8e872da457
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-22 18:15:04 -0300

    feat(web): Create the disabled property to the IconButton component

--- packages/web/src/components/IconButton/index.tsx
+++ packages/web/src/components/IconButton/index.tsx
@@ -4,6 +4,7 @@ export type IconButtonProps = {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset" | undefined;
   className?: string;
+  disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
@@ -11,13 +12,15 @@ function IconButton({
   children,
   type = "button",
   className,
+  disabled,
   onClick,
 }: IconButtonProps) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`text-center align-middle p-2 rounded-full transition-all ${className}`}
+      disabled={disabled}
+      className={`text-center align-middle p-2 rounded-full transition-all ${disabled ? "[&_*]:opacity-50" : ""} ${className}`}
     >
       {children}
     </button>
