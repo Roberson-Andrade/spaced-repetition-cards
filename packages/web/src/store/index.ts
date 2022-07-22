ec520cca20d28586255bc0e6483f382db3b50c9a
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-22 18:18:23 -0300

    refactor(web): Use callback functions to handle the loading and notifications and use axios instead of fetch

--- packages/web/src/store/index.ts
+++ packages/web/src/store/index.ts
@@ -1,9 +1,20 @@
+import axios from "axios";
 import create from "zustand";
 import { CardData, CardType, Deck } from "../types";
 
 type deckState = {
   decks: Deck[] | [];
   cards: CardType[] | []
+  fetchDecks: (callback: (error: string | null) => void) => void;
+  createDeck: (
+    payload: { name: string, category: string },
+    callback: (error: string | null, data: boolean) => void
+  ) => void;
+  deleteDeck: (
+    deckId: string,
+    callback: (error: string | null) => void
+  ) => void;
+  createCard: (cardData: CardData, callback: (error: string | null) => void) => void;
   fetchDecks: () => void;
   createDeck: (name: string, category: string) => void;
   deleteDeck: (deckId: string) => void;
@@ -13,47 +24,60 @@ type deckState = {
 export const useStore = create<deckState>()((set) => ({
   decks: [],
   cards: [],
-  fetchDecks: async () => {
-    const response = await fetch("http://127.0.0.1:3000/decks");
-    const decks: Deck[] = await response.json();
+  fetchDecks: async (callback) => {
+    try {
+      const { data } = await axios.get("http://127.0.0.1:3000/decks");
+      const decks: Deck[] = data;
     const cards = decks.map((deck) => deck.cards).flat();
-
+      callback(null);
     set({ decks, cards: cards as CardType[] });
+    } catch (error) {
+      callback((error as Error).message);
+    }
   },
-  createDeck: async (name, category) => {
-    const bodyContent = JSON.stringify({
-      name,
-      category
-    });
-
-    const response = await fetch("http://127.0.0.1:3000/decks", {
+  createDeck: async (deckData, callback) => {
+    const reqOptions = {
+      url: "http://127.0.0.1:3000/decks",
       method: "POST",
-      headers: { "Content-type": "application/json; charset=UTF-8" },
-      body: bodyContent,
-    });
+      data: deckData
+    };
 
-    const createdDeck = await response.json();
+    try {
+      const { data: createdDeck } = await axios.request(reqOptions);
 
     set((state) => ({ decks: [...state.decks, createdDeck] }));
+      callback(null, true);
+    } catch (error) {
+      callback((error as Error).message, false);
+    }
   },
-  deleteDeck: async (deckId: string) => {
-    await fetch(`http://127.0.0.1:3000/decks/${deckId}`, {
-      method: "DELETE",
-    });
+  deleteDeck: async (deckId: string, callback) => {
+    const reqOptions = {
+      url: `http://127.0.0.1:3000/decks/${deckId}`,
+      method: "DELETE"
+    };
+    try {
+      await axios.request(reqOptions);
 
-    set((state) => ({ decks: state.decks.filter((deck) => deck.id !== deckId) }));
+      callback(null);
+      set((state) => ({
+        decks: state.decks.filter((deck) => deck.id !== deckId),
+        cards: state.cards.filter((card) => card.deckId !== deckId)
+      }));
+    } catch (error) {
+      callback((error as Error).message);
+    }
   },
-  createCard: async (cardData) => {
-    const bodyContent = JSON.stringify(cardData);
-
-    const response = await fetch("http://127.0.0.1:3000/cards", {
+  createCard: async (cardData, callback) => {
+    const reqOptions = {
+      url: "http://127.0.0.1:3000/cards",
       method: "POST",
-      headers: { "Content-type": "application/json; charset=UTF-8" },
-      body: bodyContent,
-    });
+      data: cardData,
+    };
+    try {
+      const { data: createdCard } = await axios.request(reqOptions);
 
-    const createdCard = await response.json();
-
+      callback(null);
     set((state) => {
       const newDeckArray = [...state.decks];
       const deckIndex = newDeckArray.findIndex((deck) => deck.id === cardData.deckId);
@@ -67,5 +91,9 @@ export const useStore = create<deckState>()((set) => ({
 
       return ({ decks: newDeckArray, cards: [...state.cards, createdCard] });
     });
+    } catch (error) {
+      callback((error as Error).message);
+    }
+  },
   }
 }));
