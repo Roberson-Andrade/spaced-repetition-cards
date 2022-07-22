7f98c9b290be41354a42db6c542609c580150b75
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-22 18:15:55 -0300

    feat(web): Use the same bg color in the containers

--- packages/web/src/containers/MyCards/page.tsx
+++ packages/web/src/containers/MyCards/page.tsx
@@ -35,7 +35,7 @@ function MyCards() {
 
   return (
     <>
-      <div className="flex flex-grow flex-col gap-3 bg-[#F7F8FA] media-sidebar p-2 2sm:pb-16">
+      <div className="flex flex-grow flex-col gap-3 media-sidebar p-2 2sm:pb-16 bg-gradient-to-r backdrop-blur-xl from-[#e2e3e4] to-[#acbecd]">
         <header className="flex items-center gap-6 2sm:gap-2 justify-between">
           {screenWidth > 500 && <h2 className="text-2xl font-semibold">Cards</h2>}
           <Input
@@ -50,25 +50,27 @@ function MyCards() {
             {screenWidth > 550 ? "Criar Cards" : <HiOutlinePlusSm size="25px" />}
           </Button>
         </header>
-        <div className="grid auto-rows-auto grid-cols-autoFitCard gap-3 md:justify-center">
-          {cards.filter(filterBySearchValue).map(({
-            front,
-            back,
-            id,
-            deckName,
-            tag,
-            createdAt
-          }) => (
-            <Card
-              key={id}
-              front={front}
-              back={back}
-              deckName={deckName}
-              tag={tag}
-              createdAt={createdAt}
-              className="h-[300px]"
-            />
-          )) }
+        <div className="max-h-[90vh] sm:max-h-[85vh] overflow-y-auto">
+          <div className="grid auto-rows-auto grid-cols-autoFitCard gap-3 md:justify-center p-3">
+            {cards.filter(filterBySearchValue).map(({
+              front,
+              back,
+              id,
+              deckName,
+              tag,
+              createdAt
+            }) => (
+              <Card
+                key={id}
+                front={front}
+                back={back}
+                deckName={deckName}
+                tag={tag}
+                createdAt={createdAt}
+                className="h-[300px]"
+              />
+            )) }
+          </div>
         </div>
       </div>
       <FormModal open={openModal}>
--- packages/web/src/containers/MyDecks/page.tsx
+++ packages/web/src/containers/MyDecks/page.tsx
@@ -4,7 +4,7 @@ import DeckTable from "./DeckTable";
 function MyDecks() {
   const deckItems = useStore((state) => state.decks);
   return (
-    <div className="flex justify-center flex-grow bg-[#F7F8FA]">
+    <div className="flex justify-center flex-grow bg-gradient-to-r backdrop-blur-xl from-[#e2e3e4] to-[#acbecd]">
       <DeckTable items={deckItems} />
     </div>
   );
