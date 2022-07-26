950a82ad7be1a90d9306e239bd09ac44e790e55e
Roberson Andrade <roberson_souza11@hotmail.com>
2022-07-26 18:40:24 -0300

    feat(server): Remove the updateCardRevision useCase

--- packages/server/src/useCases/card/updateCardRevision/index.ts
+++
@@ -1,9 +0,0 @@
-import { PostgresCardRepository } from '../../../repositories/implementations/PostgresCardRepository';
-import { UpdateCardRevision } from './UpdateCardRevision';
-import { UpdateCardRevisionController } from './UpdateCardRevisionController';
-
-const postgresCardRepository = new PostgresCardRepository();
-
-const updateCardRevision = new UpdateCardRevision(postgresCardRepository);
-
-export const updateCardRevisionController = new UpdateCardRevisionController(updateCardRevision);
