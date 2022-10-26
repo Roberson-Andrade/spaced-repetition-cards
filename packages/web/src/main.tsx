2c7e97ae5f32d0fa703946a671af2a6f6299b9cc
Roberson Andrade <roberson_souza11@hotmail.com>
2022-10-25 23:07:30 -0300

    feat(web): install msw and setup

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { worker } from "./mocks/browser";
+
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
