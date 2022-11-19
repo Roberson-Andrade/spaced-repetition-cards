2f841ee457ae70d2876c014cea0bf0df0c3ca72e
Roberson Andrade <roberson_souza11@hotmail.com>
2022-11-19 18:27:46 -0300

    test(web): Add MyDecks test case

--- packages/web/src/containers/MyDecks/MyDecks.spec.tsx
+++ packages/web/src/containers/MyDecks/MyDecks.spec.tsx
@@ -1,32 +1,69 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  render, waitFor
+} from "@testing-library/react";
 import { setupServer } from "msw/node";
+import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { ToastContainer } from "react-toastify";
 import { handlers } from "../../mocks/handlers";
-import MyDecks from "./page";
+import App from "../../App";
 
 const server = setupServer(
   handlers[0],
   handlers[3]
 );
 
-beforeAll(() => server.listen());
+beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe("MyDecks Container", () => {
-  it("Should add a deck to the list", () => {
-    const { getByText } = render(
-      <>
-        <MyDecks />
+  it("Should add a deck to the list and show success snackbar", async () => {
+    const route = "/mydecks";
+
+    const {
+      getByText, getByLabelText, findByText, queryByText
+    } = render(
+      <MemoryRouter initialEntries={[route]}>
+        <App />
+        <ToastContainer />
         <div id="modal" />
-      </>
+      </MemoryRouter>
     );
 
-    const openModalButton = getByText("Criar deck");
+    const openModalButton = await findByText("Criar deck");
 
-    fireEvent.click(openModalButton);
+    expect(queryByText("Economia")).not.toBeInTheDocument();
 
-    screen.debug();
+    await userEvent.click(openModalButton);
+    await userEvent.type(getByLabelText("Nome"), "Test");
+    await userEvent.type(getByLabelText("Categoria"), "Test");
+    await userEvent.click(getByText("Criar"));
 
-    expect(getByText("Voltar")).toBeInTheDocument();
+    await waitFor(() => expect(getByText("Economia")).toBeInTheDocument());
+    waitFor(() => expect(getByText("Deck deletado com sucesso!")).toBeInTheDocument);
+  });
+
+  it("Should remove a deck from the list and show success snackbar", async () => {
+    const route = "/mydecks";
+
+    const {
+      queryByText, findByText, getAllByTestId, getByText
+    } = render(
+      <MemoryRouter initialEntries={[route]}>
+        <App />
+        <ToastContainer />
+      </MemoryRouter>
+    );
+
+    const itemToDelete = await findByText("História");
+    const deleteBtnList = getAllByTestId("icon-button");
+
+    expect(itemToDelete).toBeInTheDocument();
+
+    await userEvent.click(deleteBtnList[1]);
+
+    waitFor(() => expect(queryByText("História")).not.toBeInTheDocument);
+    waitFor(() => expect(getByText("Deck deletado com sucesso!")).toBeInTheDocument);
   });
 });
